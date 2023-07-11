2d9e60a365a10ffa6ce96a1f74fdc6f2971a6e32
harikaboppudi10 <dyava.2@wright.edu>
2023-07-11 13:00:24 -0400

    User registration API changes added service implementation code

---
+++ server/src/main/java/com/backend/server/service/UserService.java
@@ -0,0 +1,18 @@
+package com.backend.server.service;
+
+import com.backend.server.Model.Inventory;
+import com.backend.server.Model.User;
+
+import java.util.Optional;
+
+
+public interface UserService {
+    public User addUser(User user);
+    public Optional<User> getUser(String email);
+
+    User updateUser(User user);
+
+    String deleteUser(String mobileNumber);
+
+//    Inventory addItemToCart(Inventory inventory);
+}
