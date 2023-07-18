f8f7814775b97b0da83fbd6bff2f249af45570a0
yatasrivaishnavi <133189016+yatasrivaishnavi@users.noreply.github.com>
2023-07-18 11:01:49 -0500

    Update CurrentUserDetails.java

--- server/src/main/java/com/backend/server/service/CurrentUserDetails.java
+++ server/src/main/java/com/backend/server/service/CurrentUserDetails.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
+@Service("userDetailsService")
 public class CurrentUserDetails implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
