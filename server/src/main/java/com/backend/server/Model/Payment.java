292f8e42f8052750093c0a34781d77e57ad4078e
chitrabhanu-ponna <134437432+chitrabhanu-ponna@users.noreply.github.com>
2023-07-25 11:11:01 -0500

    Added payment mode selection

---
+++ server/src/main/java/com/backend/server/Model/Payment.java
@@ -0,0 +1,28 @@
+package com.backend.server.Model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "user")
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Payment.java {
+    
+    @Id
+    private String paymentId;
+    private String paymentMode;
+    private String amount;
+    private String Status;
+
+}
