639cb90fa2ea9c3b71431bee8ef458a02eb40c09
Niharika Poreddy <133179545+niharika-poreddy@users.noreply.github.com>
2023-07-27 16:12:55 -0400

    Update InventoryRepository.java
    Added comments, issue, literate programming and Program by contract to InventoryRepository.java

--- server/src/main/java/com/backend/server/repository/InventoryRepository.java
+++ server/src/main/java/com/backend/server/repository/InventoryRepository.java
@@ -1,9 +1,14 @@
-package com.backend.server.repository;
+package com.backend.server.repository; // Package declaration for the "repository" package under "com.backend.server"
 
-import com.backend.server.Model.Inventory;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+import com.backend.server.Model.Inventory; // Import the Inventory model class
+import org.springframework.data.mongodb.repository.MongoRepository; // Import Spring Data MongoRepository
+import org.springframework.stereotype.Repository; // Import Spring annotation for repository
 //Repository to store and make changes in db for Inventory table
-@Repository
+@Repository // Indicates that this interface is a Spring repository
 public interface InventoryRepository extends MongoRepository<Inventory,Integer> {
+  /** @brief The MongoRepository interface provides CRUD operations for the Inventory model
+  * It is parameterized with <Inventory, Integer> where Inventory is the model class and Integer is the data type of the primary key (itemId in this case)
+  * This interface does not require any additional methods since it inherits all the necessary CRUD operations from MongoRepository
+  * See [Issue13] (https://github.com/WSUCEG-7140/mystore-teamDelta/issues/13)
+  */
 }
