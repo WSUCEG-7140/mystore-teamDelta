a555338f0e91ac1d8168213628d8f43a42e1e9e3
Niharika Poreddy <133179545+niharika-poreddy@users.noreply.github.com>
2023-07-27 13:27:28 -0400

    Update InventoryRepository.java

--- server/src/main/java/com/backend/server/repository/InventoryRepository.java
+++ server/src/main/java/com/backend/server/repository/InventoryRepository.java
@@ -3,7 +3,7 @@ package com.backend.server.repository;
 import com.backend.server.Model.Inventory;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
+//Repository to store and make changes in db for Inventory table
 @Repository
 public interface InventoryRepository extends MongoRepository<Inventory,Integer> {
 }
