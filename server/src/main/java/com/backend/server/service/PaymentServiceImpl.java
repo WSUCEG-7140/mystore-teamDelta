2a5b9641de55b381b11378f5008064ac54458d47
Niharika Poreddy <133179545+niharika-poreddy@users.noreply.github.com>
2023-07-27 17:50:54 -0400

    Update PaymentServiceImpl.java
    Added comments, Issue, Literate programing and Program by contract to PaymentServiceImpl.java

--- server/src/main/java/com/backend/server/service/PaymentServiceImpl.java
+++ server/src/main/java/com/backend/server/service/PaymentServiceImpl.java
@@ -1,12 +1,20 @@
-package com.backend.server.service;
+package com.backend.server.service; // Package declaration for the "service" package under "com.backend.server"
 
-import com.backend.server.Model.Payment;
-import com.backend.server.repository.PaymentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
+import com.backend.server.Model.Payment; // Import the Payment model class
+import com.backend.server.repository.PaymentRepository; // Import the PaymentRepository interface
+import org.springframework.beans.factory.annotation.Autowired; // Import Spring annotation for dependency injection
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; // Import Spring Security class for BCrypt password encoder
+import org.springframework.stereotype.Service; // Import Spring annotation for service
 
-import java.util.Optional;
+import java.util.Optional; // Import Java utility class for Optional
+
+/**
+ * @class PaymentServiceImpl
+ * @brief This class implements the PaymentService interface and provides the implementation for payment management.
+ *
+ * @Service Indicates that this class is a Spring service
+ * @Autowired Enables automatic injection of a PaymentRepository bean
+ */
 
 @Service
 public class PaymentServiceImpl implements PaymentService{
@@ -14,8 +22,17 @@ public class PaymentServiceImpl implements PaymentService{
     @Autowired
     PaymentRepository paymentRepository;
 
-
     @Override
+
+    /**
+     * addPayment Method to add a new payment to the system.
+     * The method takes a Payment object as input and returns the added Payment object.
+     *
+     * Save the new payment using the PaymentRepository.
+     * @Return the added payment object.
+     * See [Issue33] (https://github.com/WSUCEG-7140/mystore-teamDelta/issues/33)
+     */
+    
     public Payment addPayment(Payment payment) {
        paymentRepository.save(payment);
        return payment;
