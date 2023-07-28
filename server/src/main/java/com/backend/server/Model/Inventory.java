a21a9e754d0e3f929377992974aeb48f48019775
Niharika Poreddy <133179545+niharika-poreddy@users.noreply.github.com>
2023-07-28 02:52:50 -0400

    Update Inventory.java
    Added Literate programming and Program by contract in Inventory.java

--- server/src/main/java/com/backend/server/Model/Inventory.java
+++ server/src/main/java/com/backend/server/Model/Inventory.java
@@ -14,6 +14,16 @@ import org.springframework.data.mongodb.core.mapping.Document; // Import Spring 
 @Builder // Lombok annotation to generate builder pattern
 @Document(collection = "inventory") // MongoDB document mapping for the "inventory" collection
 @JsonInclude(JsonInclude.Include.NON_NULL) // Jackson annotation to exclude properties with null values from JSON serialization
+
+/**
+ * @class Inventory
+ * @brief This class represents an Inventory with its ID ,name, category, cost and supplier.
+ *
+ * The Inventory class encapsulates information about an Inventory, including its unique
+ * Inventory code (itemID) and its name, category, cost, supplier. Objects of this class can be used to
+ * store and retrieve Inventory-related information in the Mystore application.
+ */
+    
 public class Inventory {
     @Id // Marks the field below as the identifier field for MongoDB
     private int itemId; // Unique identifier for the inventory item
