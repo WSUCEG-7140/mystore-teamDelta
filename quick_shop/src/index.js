1679dc78ae517e6154222f988ac91803901a9a8a
Niharika Poreddy <133179545+niharika-poreddy@users.noreply.github.com>
2023-07-28 11:41:19 -0400

    Update index.js

--- quick_shop/src/index.js
+++ quick_shop/src/index.js
@@ -1,8 +1,10 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './App'; // Import the App component from the './App' file
+import './index.css'; // Import the styles from the './index.css' file
 
+// Render the App component inside the HTML element with the ID 'root'
 ReactDOM.render(
   <App />,
   document.getElementById('root')
