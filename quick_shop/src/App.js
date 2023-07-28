edf0d115d2b66ca531491931bff454e1e59de550
Niharika Poreddy <133179545+niharika-poreddy@users.noreply.github.com>
2023-07-28 11:46:59 -0400

    Update App.js

--- quick_shop/src/App.js
+++ quick_shop/src/App.js
@@ -1,8 +1,11 @@
+// Import the CSS styles from the "App.css" file
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";  
 
+// Define the main App component
 function App() {
+  // Create a parent container div with the class "App" for styling purposes
   return (
     <div className="App">
       <ShopContextProvider>
